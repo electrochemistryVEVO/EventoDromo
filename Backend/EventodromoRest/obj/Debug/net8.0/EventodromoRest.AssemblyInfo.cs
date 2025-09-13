2a9e4a8c52e66db24ba2e62041075bad823e43ac
TadashiRKS <a20201256@pucp.edu.pe>
2025-09-13 14:34:11 -0500

    Prueba de funcionamiento del proyecto API - sin cambios

--- Backend/EventodromoRest/obj/Debug/net8.0/EventodromoRest.AssemblyInfo.cs
+++ Backend/EventodromoRest/obj/Debug/net8.0/EventodromoRest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventodromoRest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45883a8eaf3b1561043827964526c6e94e44e73c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventodromoRest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventodromoRest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
