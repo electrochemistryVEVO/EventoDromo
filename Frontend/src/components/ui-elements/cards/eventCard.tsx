1a7ef4810cac4f2a78cd3f94ad59a0938029f251
electrochemistryVEVO <edwardxvi09@gmail.com>
2025-10-05 09:14:54 -0500

    avance de lista de eventos

---
+++ Frontend/src/components/ui-elements/cards/eventCard.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { Suspense,use } from "react";
+import triangleRight from "@/assets/triangleRignt.png"
+
+type Evento = {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  idTipoEvento: number;
+  idLocal: number;
+  creadoPor: number;
+  fechaPublicacion: Date;
+  fechaCompra: Date;
+  isDeleted: number;
+  imagenURL: string;
+}
+type PropsWithEvento = {
+  event: Evento;
+}
+export default function EventCard(props:PropsWithEvento){
+  let evento : Evento = props.event;
+  const img = import('@/assets/pictures/'+evento.imagenURL)
+  let fechaPublicacionFormat : string = new Intl.DateTimeFormat("es-419", {
+    dateStyle: "full"
+  }).format(evento.fechaPublicacion)
+  return (<div className="col mb-5">
+    <div className="card h-100">
+      <Suspense>
+        <Image
+          className="card-img-top"
+          src={use(img)}
+          alt="..."
+        />
+      </Suspense>
+      <div className="row card-body p-4">
+        <Image className="col-2" src={triangleRight} alt={"ok"}/>
+        <div className="col-md-auto text-center">
+          <h5 className="fw-bolder">{evento.nombre}</h5>
+          {fechaPublicacionFormat}
+        </div>
+      </div>
+      <div className="card-footer border-top-0 bg-transparent p-4 pt-0">
+        <div className="text-center">
+          <a className="btn btn-outline-dark mt-auto" href="#">
+            View options
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>);
+}
