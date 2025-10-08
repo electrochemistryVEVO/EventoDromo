e5ed8cbef270262feb73d84a6f58a3325ebefb19
Moises <varasaugusto191@gmail.com>
2025-10-08 13:01:32 -0500

    Update Modelo.ts
    modelos Cliente, Sexo, TipoDocumento, Ciudad, Pais .ts

--- Frontend/src/types/Modelo.ts
+++ Frontend/src/types/Modelo.ts
@@ -1,3 +1,43 @@
 export type Cliente = {
-    id: number;
-}
+  id?: number;
+  nombres?: string;
+  apellidos?: string;
+  email?: string;
+  passwordhash?: string;
+  fechanacimiento?: string; // Date en C#, string ISO en TS
+  idsexo?: number;
+  sexo?: Sexo;
+  idtipodocumento?: number;
+  tipodocumento?: TipoDocumento;
+  numerodocumento?: string;
+  telefono?: string;
+  idciudad?: number;
+  ciudad?: Ciudad;
+  politicadeprivacidad?: boolean;
+  enviodepublicidad?: boolean;
+  fechacreacion?: string;
+  fechaultimaedicion?: string;
+  fechaultimasession?: string;
+};
+
+export type Sexo = {
+  id?: number;
+  nombre?: string;
+};
+
+export type TipoDocumento = {
+  id?: number;
+  nombre?: string;
+};
+
+export type Ciudad = {
+  id?: number;
+  nombre?: string;
+  idPais?: number;
+  pais?: Pais;
+};
+
+export type Pais = {
+  id?: number;
+  nombre?: string;
+};
