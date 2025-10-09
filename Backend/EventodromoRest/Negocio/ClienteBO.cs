396c68b0b19608e012c8ad0f79a971627d6e18cb
Moises <varasaugusto191@gmail.com>
2025-10-09 02:22:32 -0500

    login y signUp funcionando
    ya funciona, recordar cambiar las rutas en los srvices si hacen local

--- Backend/EventodromoRest/Negocio/ClienteBO.cs
+++ Backend/EventodromoRest/Negocio/ClienteBO.cs
@@ -24,6 +24,38 @@ namespace EventodromoRest.Negocio
             
         }
 
+        public SignUpResponse InsertarCliente(RequestSignUpCliente request)
+        {
+            var mapper = new ClienteMapper(globales, DB);
+            /* algo así hacer
+            Cliente nuevoCliente = new Cliente
+            {
+                nombres = request.nombres,
+                apellidos = request.apellidos,
+                email = request.email,
+                passwordhash = globales.HashPassword(request.password),
+                fechanacimiento = request.fechaNacimiento,
+                idsexo = mapper.ObtenerIdSexoPorNombre(request.sexo),
+                idtipodocumento = mapper.ObtenerIdTipoDocumentoPorNombre(request.tipoDocumento),
+                numerodocumento = request.numeroDocumento,
+                telefono = request.telefono,
+                idciudad = mapper.ObtenerIdCiudadPorNombreYPais(request.ciudad, request.pais),
+                politicadeprivacidad = request.politicadeprivacidad,
+                enviodepublicidad = request.enviodepublicidad,
+                fechacreacion = DateTime.Now,
+                fechaultimaedicion = DateTime.Now,
+                fechaultimasession = null
+            };
+            
+            int newId = mapper.InsertarCliente(nuevoCliente);
+            */
+            SignUpResponse signUpResponse = new SignUpResponse
+            {
+                //success = newId > 0
+                success = true // temporal mientras no se implemente todo
+            };
+            return signUpResponse;
+        }
 
 
     }
