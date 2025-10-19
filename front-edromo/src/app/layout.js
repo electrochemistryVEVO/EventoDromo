8fdc0c510ab7fb8893334f20596bcd99bb1a12af
DivanoPerezNina <a20221481@pucp.edu.pe>
2025-10-19 18:30:28 -0500

    archivos

---
+++ front-edromo/src/app/layout.js
@@ -0,0 +1,26 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Eventodromo",
+  description: "Proyecto ingenieria de software",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+
+      {children}
+
+    </html>
+  );
+}
