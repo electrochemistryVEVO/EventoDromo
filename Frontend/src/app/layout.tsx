07608129d834e5d720d87665fe93d494fed7ebec
TadashiRKS <a20201256@pucp.edu.pe>
2025-10-23 21:35:43 -0500

    Se eliminaron elementos innecesarios del layout padre.

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,33 +1,10 @@
-import "@/css/satoshi.css";
 import "@/css/style.css";
-
-import { Sidebar } from "@/components/Layouts/sidebar";
-
-import "flatpickr/dist/flatpickr.min.css";
-import "jsvectormap/dist/jsvectormap.css";
-
-import { Header } from "@/components/Layouts/header";
-import type { Metadata } from "next";
-import NextTopLoader from "nextjs-toploader";
 import type { PropsWithChildren } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import '@/css/styles.css';
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s | NextAdmin - Next.js Dashboard Kit",
-    default: "NextAdmin - Next.js Dashboard Kit",
-  },
-  description:
-    "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
-};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html>
-      <body>
-        {children}
-      </body>
+      <body> {children} </body>
     </html>
   );
 }
