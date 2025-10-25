521b232689a68e91360ec1a503a8b9e5ad7fc59d
Marcelo <a20191204@pucp.edu.pe>
2025-10-25 16:51:15 -0500

    flujo hasta login

--- front-edromo/src/services/loginService.js
+++ front-edromo/src/services/loginService.js
@@ -1,7 +1,7 @@
 export async function autenticarUsuario(loginInfo) {
   //link q funciona en individual: http://localhost:5189/api/Cliente/AutenticarLoginCliente"
   //link q funciona en docker: http://localhost:8081/api/Cliente/AutenticarLoginCliente"
-  /*const res = await fetch("http://localhost:5189/api/Cliente/AutenticarLoginCliente", {
+  const res = await fetch("http://localhost:5189/api/Cliente/AutenticarLoginCliente", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -15,7 +15,7 @@ export async function autenticarUsuario(loginInfo) {
   if(json.success===false){
     throw new Error(json.error || json.mensaje || "Error en respuesta del servicio");
   }
-  return json.data; */
+  return json.data; 
 
   // Simulación de una llamada al backend
   console.log(" MODO HARDCODEADO: Devolviendo respuesta de login simulada.");
