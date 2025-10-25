19b8e145f5396f3a7dd0fb7d5f00b8077f8f32f1
Moises <varasaugusto191@gmail.com>
2025-10-25 16:35:18 -0500

    home eventos lista
    se creó page.js en app y este redirige a el page.js en user/eventos/lista

---
+++ front-edromo/src/app/page.js
@@ -0,0 +1,15 @@
+/*esta es una page.js que se abre por default por el next.js cuando corre http://localhost:3000/
+se abre este page.js entonces aqui mismo estoy redigiriendo a la pagina principal que es /user/eventos/lista */
+// app/page.js
+
+import { redirect } from 'next/navigation';
+
+export default function RootPage() {
+  // Cuando este componente se renderiza (al acceder a /),
+  // Next.js inmediatamente redirige al usuario.
+  redirect('/user/eventos/lista');
+  
+  // Opcionalmente, puedes retornar null o un fragmento vacío
+  // aunque 'redirect' detiene el renderizado.
+  // return null; 
+}
