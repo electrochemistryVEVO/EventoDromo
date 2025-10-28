bf9ea7063b0b49ee868bb1f15d1aa8addc632b82
Moises <varasaugusto191@gmail.com>
2025-10-28 18:33:21 -0500

    servicio FetchAdminData
    ya esta servicio en backend

--- Backend/EventodromoRest/Negocio/AdministradorBO.cs
+++ Backend/EventodromoRest/Negocio/AdministradorBO.cs
@@ -5,6 +5,29 @@ namespace EventodromoRest.Negocio
 {
     public class AdministradorBO(Globales.Globales globales, DBManager.DBManager DB)
     {
-        
+        public FetchUserDataResponse ObtenerNombrePorId(int idAdmin)
+        {
+            var mapper = new AdministradorMapper(globales, DB);
+            Administrador administrador = mapper.ObtenerAdministradorPorId(idAdmin);
+            if (administrador != null)
+            {
+                return new FetchUserDataResponse
+                {
+                    status = "success",
+                    message = "Admin encontrado.",
+                    name = $"{administrador.nombres} {administrador.apellidos}"
+                };
+            }
+            else
+            {
+                return new FetchUserDataResponse
+                {
+                    status = "error",
+                    message = "Admin no encontrado.",
+                    name = null
+                };
+            }
+
+        }
     }
 }
