44ec09ee186bd1620b328c1a1a15a212b849b120
DivanoPerezNina <a20221481@pucp.edu.pe>
2025-10-31 19:32:21 -0500

    duplicados

--- Backend/EventodromoRest/Modelos/Evento.cs
+++ Backend/EventodromoRest/Modelos/Evento.cs
@@ -42,16 +42,6 @@
         public required int idEvento { get; set; }
     }
 
-    public class RequestListarEventosPorBusqueda
-    {
-        public required string busqueda { get; set; }
-    }
-
-    public class RequestObtenerEventoPorId
-    {
-        public required int idEvento { get; set; }
-    }
-
     public class ResponseListarEventosYLocales
     {
         public List<EventosLocalCiudadCategoriaDTO> eventos { get; set; }
