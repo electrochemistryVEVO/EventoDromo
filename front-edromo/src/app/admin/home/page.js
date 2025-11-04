ca2c7693aa000f0599129b3fdc1726ba319d4d97
TadashiRKS <a20201256@pucp.edu.pe>
2025-11-03 22:49:40 -0500

    El navbar funciona correctamente

---
+++ front-edromo/src/app/admin/home/page.js
@@ -0,0 +1,13 @@
+// Ruta del archivo: src/app/admin/page.js
+
+export default function AdminHomePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Bienvenidos al home del admin</h1>
+      <p>
+        Desde aquí podrás gestionar los eventos, usuarios y demás
+        configuraciones del sitio.
+      </p>
+    </main>
+  );
+}
---
+++ front-edromo/src/app/auth/login/controller.js
@@ -0,0 +1,41 @@
+"use client";
+
+import { autenticarUsuario } from "@/services/Login.service.js";
+
+export async function onSubmit(formData) {
+  try {
+    const loginInfo = {
+      Correo: formData.get("email"),
+      Password: formData.get("password"),
+    };
+
+    const response = await autenticarUsuario(loginInfo);
+
+    if (response.success) {
+      const rol = response.rol;
+      const token = response.token; // 🔹 Capturamos el token recibido del backend
+
+      // Guardamos toda la info de sesión
+      sessionStorage.setItem(
+        "session",
+        JSON.stringify({
+          rol,
+          token, // 🔹 Guardamos el token
+        })
+      );
+
+      // 👇 devolvemos TODO lo que el componente necesita:
+      return {
+        success: true,
+        rol,
+        token, // 🔹 Lo devolvemos también al page.js
+      };
+    }else {
+      // ❌ Si el backend respondió que las credenciales son incorrectas:
+      return { error: "Credenciales inválidas" };
+    }
+  } catch (error) {
+    console.error("Error en el login:", error);
+    return { error: "Error al intentar iniciar sesión" };
+  }
+}
