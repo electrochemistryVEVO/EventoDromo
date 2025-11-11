04bf97e4419841c4deeddc46f5964abdd48bf7ec
electrochemistryVEVO <edwardxvi09@gmail.com>
2025-11-11 00:56:51 -0500

    coldfix

--- front-edromo/src/lib/api.js
+++ front-edromo/src/lib/api.js
@@ -16,6 +16,8 @@ async function apiFetch(endpoint, options = {}) {
     const baseHeaders = {
         'Content-Type': 'application/json',
         "Access-Control-Allow-Origin":"*",
+        "Access-Control-Allow-Methods":"GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
         ...options.headers,
     };
 
