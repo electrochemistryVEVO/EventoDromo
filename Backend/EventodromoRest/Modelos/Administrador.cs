c8a77a63de99b19899e6f0cccfb8af76cbe3a9ab
TadashiRKS <a20201256@pucp.edu.pe>
2025-11-26 00:39:53 -0500

    Se agrego el servicio para la obtencion de indicadores. No se logro obtener el indicador de tiempo de sesion promedio porque no hay informacion sobre esto en la base de datos

--- Backend/EventodromoRest/Modelos/Administrador.cs
+++ Backend/EventodromoRest/Modelos/Administrador.cs
@@ -9,4 +9,19 @@
         public string? passwordHash { get; set; }
         public DateTime? fechaCreacion { get; set; }
     }
+
+    public class MetricasDashboardDTO
+    {
+        public MetricaConMontoDTO ingresosTotales { get; set; }
+        public MetricaConMontoDTO puntosUsadosPromedio { get; set; }
+        public MetricaConMontoDTO entradasVendidas { get; set; }
+        public MetricaConMontoDTO usuariosNuevos { get; set; }
+        public MetricaConMontoDTO tasaConversion { get; set; }
+    }
+
+    public class MetricaConMontoDTO
+    {
+        public decimal valor { get; set; }
+        public decimal porcentajeCambio { get; set; }
+    }
 }
