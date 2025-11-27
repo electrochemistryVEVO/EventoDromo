4ec4788627c70d53e449d28d490b52b88bc29578
Moises <varasaugusto191@gmail.com>
2025-11-26 19:11:33 -0500

    correcion nextconfig ejemplo.com

--- front-edromo/next.config.mjs
+++ front-edromo/next.config.mjs
@@ -48,6 +48,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'eventodromo-s3.s3.amazonaws.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'ejemplo.com',
+      },
     ],
   },
 
